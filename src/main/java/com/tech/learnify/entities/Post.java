ae274def37107824d01bb6e3152352aec3d028e1
Shailendu95 <cshailendu@gmail.com>
2024-09-05 22:22:16 +0530

    I created 2 java class file,that is Category.java and Post.java  .

---
+++ src/main/java/com/tech/learnify/entities/Post.java
@@ -0,0 +1,95 @@
+package com.tech.learnify.entities;
+
+import java.sql.Date;  
+
+public class Post {
+    private int pid;
+    private String pTitle;
+    private String pContent;
+    private String pCode;
+    private String pPic;
+    private Date pDate; 
+    private int catId;
+
+   
+    public Post() {
+    }
+
+   
+    public Post(int pid, String pTitle, String pContent, String pCode, String pPic, Date pDate, int catId) {
+        this.pid = pid;
+        this.pTitle = pTitle;
+        this.pContent = pContent;
+        this.pCode = pCode;
+        this.pPic = pPic;
+        this.pDate = pDate;
+        this.catId = catId;
+    }
+
+    
+    public Post(String pTitle, String pContent, String pCode, String pPic, Date pDate, int catId) {
+        this.pTitle = pTitle;
+        this.pContent = pContent;
+        this.pCode = pCode;
+        this.pPic = pPic;
+        this.pDate = pDate;
+        this.catId = catId;
+    }
+
+    
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public String getpTitle() {
+        return pTitle;
+    }
+
+    public void setpTitle(String pTitle) {
+        this.pTitle = pTitle;
+    }
+
+    public String getpContent() {
+        return pContent;
+    }
+
+    public void setpContent(String pContent) {
+        this.pContent = pContent;
+    }
+
+    public String getpCode() {
+        return pCode;
+    }
+
+    public void setpCode(String pCode) {
+        this.pCode = pCode;
+    }
+
+    public String getpPic() {
+        return pPic;
+    }
+
+    public void setpPic(String pPic) {
+        this.pPic = pPic;
+    }
+
+    public Date getpDate() {
+        return pDate;
+    }
+
+    public void setpDate(Date pDate) {
+        this.pDate = pDate;
+    }
+
+    public int getCatId() {
+        return catId;
+    }
+
+    public void setCatId(int catId) {
+        this.catId = catId;
+    }
+}
